ff2fa8e9c41a27e79174b6251a75da5ad7cd0cb8
mog <mog@rldn.net>
2026-08-18 11:05:36 -0400

    Disable shuttle pointer via libinput on Wayland

--- xinput.go
+++ xinput.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"regexp"
 	"time"
@@ -12,25 +13,57 @@ var xinputDevices = []*regexp.Regexp{
 	regexp.MustCompile(`↳ Contour Design ShuttlePRO v2\s+id=(\d+)\s`),
 }
 
+var libinputDevices = []*regexp.Regexp{
+	regexp.MustCompile(`path: /dev/input/(\S+)\s+.*ShuttlePRO`),
+}
+
+func isWayland() bool {
+	return os.Getenv("WAYLAND_DISPLAY") != ""
+}
+
 func disableXInputPointer() {
 	for {
-		cnt, err := exec.Command("xinput", "list").Output()
-		if err != nil {
-			log.Println("Couldn't list xinput:", err)
-			goto end
-		}
-
-		for _, dev := range xinputDevices {
-			matches := dev.FindStringSubmatch(string(cnt))
-			if matches == nil {
-				continue
+		if isWayland() {
+			// On Wayland, use libinput to disable the shuttle pointer
+			out, err := exec.Command("libinput", "list-devices").Output()
+			if err != nil {
+				log.Println("Couldn't list libinput devices:", err)
+				goto end
 			}
 
-			id := matches[1]
-			fmt.Println("Disabling XInput id:", id)
-			if err := exec.Command("xinput", "disable", id).Run(); err != nil {
-				log.Println("Couldn't disable xinput device:", err)
+			for _, dev := range libinputDevices {
+				matches := dev.FindStringSubmatch(string(out))
+				if matches == nil {
+					continue
+				}
+
+				devicePath := "/dev/input/" + matches[1]
+				fmt.Println("Disabling libinput device:", devicePath)
+				if err := exec.Command("libinput", "disable", devicePath).Run(); err != nil {
+					log.Println("Couldn't disable libinput device:", err)
+					goto end
+				}
+			}
+		} else {
+			// On X11, use xinput to disable the shuttle pointer
+			cnt, err := exec.Command("xinput", "list").Output()
+			if err != nil {
+				log.Println("Couldn't list xinput:", err)
 				goto end
+			}
+
+			for _, dev := range xinputDevices {
+				matches := dev.FindStringSubmatch(string(cnt))
+				if matches == nil {
+					continue
+				}
+
+				id := matches[1]
+				fmt.Println("Disabling XInput id:", id)
+				if err := exec.Command("xinput", "disable", id).Run(); err != nil {
+					log.Println("Couldn't disable xinput device:", err)
+					goto end
+				}
 			}
 		}
 
